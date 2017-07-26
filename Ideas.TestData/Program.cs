32be6138cd79e83016634c854cb31785ba338023
Piotr Hechelski <piotrek.h@hotmail.com>
2017-07-26 22:25:47 +0200

    Created initial project structure

---
+++ Ideas.TestData/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ideas.TestData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
