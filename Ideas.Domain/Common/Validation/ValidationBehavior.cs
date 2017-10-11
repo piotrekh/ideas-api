7ba2ba9807bfdc6f06e21813e081256a90e89dd4
Piotr Hechelski <piotrek.h@hotmail.com>
2017-10-11 12:18:46 +0200

    Removed unused code

--- Ideas.Domain/Common/Validation/ValidationBehavior.cs
+++ Ideas.Domain/Common/Validation/ValidationBehavior.cs
@@ -4,59 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 
 namespace Ideas.Domain.Common.Validation
-{
-    //public class ValidationBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : IRequest<TResponse>
-    //{
-    //    private readonly IValidator<TRequest> _validator;
-
-    //    public ValidationBehavior(IValidator<TRequest> validator)
-    //    {
-    //        _validator = validator;
-    //    }
-
-    //    public Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next)
-    //    {
-    //        if (_validator == null)
-    //            return next();
-
-    //        var context = new ValidationContext(request);
-    //        var failures = _validator.Validate(context).Errors;
-
-    //        if (failures.Any())
-    //        {
-    //            throw new ValidationException(failures);
-    //        }
-
-    //        return next();
-    //    }
-    //}
-
-    //public class ValidationBehavior<TRequest> : IPipelineBehavior<TRequest, Unit> where TRequest : IRequest
-    //{
-    //    private readonly IValidator<TRequest> _validator;
-
-    //    public ValidationBehavior(IValidator<TRequest> validator)
-    //    {
-    //        _validator = validator;
-    //    }
-
-    //    public Task<Unit> Handle(TRequest request, RequestHandlerDelegate<Unit> next)
-    //    {
-    //        if (_validator == null)
-    //            return next();
-
-    //        var context = new ValidationContext(request);
-    //        var failures = _validator.Validate(context).Errors;
-
-    //        if (failures.Any())
-    //        {
-    //            throw new ValidationException(failures);
-    //        }
-
-    //        return next();
-    //    }
-    //}
-    
+{  
     public class ValidationBehavior<TRequest, TResponse> : ValidationBehaviorBase<TRequest, TResponse>
     {
         public ValidationBehavior(IValidator<TRequest> validator) : base(validator) { }
