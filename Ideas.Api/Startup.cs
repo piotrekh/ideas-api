aabbd4cb29ccff414b4f7cb71ff632bd5fe3a4f7
Piotr Hechelski <piotrek.h@hotmail.com>
2017-10-16 11:10:42 +0200

    Removed appsettings.Development.json

--- Ideas.Api/Startup.cs
+++ Ideas.Api/Startup.cs
@@ -39,7 +39,6 @@ namespace Ideas.Api
             var builder = new ConfigurationBuilder()
                 .SetBasePath(env.ContentRootPath)
                 .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
                 .AddEnvironmentVariables();
 
             builder.AddUserSecrets<Startup>();
